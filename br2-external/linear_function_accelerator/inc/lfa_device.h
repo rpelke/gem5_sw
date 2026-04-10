acd81f5a853a4d28b132734d7f620981b122c972
Rebecca Pelke <pelke@ice.rwth-aachen.de>
2026-04-10 14:23:31 +0200

    Add multi-instance char device registration
    Signed-off-by: Rebecca Pelke <pelke@ice.rwth-aachen.de>

--- br2-external/linear_function_accelerator/inc/lfa_device.h
+++ br2-external/linear_function_accelerator/inc/lfa_device.h
@@ -4,6 +4,7 @@
 #include <linux/atomic.h>
 #include <linux/cdev.h>
 #include <linux/io.h>
+#include <linux/kdev_t.h>
 #include <linux/platform_device.h>
 
 int lfa_register_device(struct platform_device *pdev);
@@ -11,6 +12,8 @@ void lfa_unregister_device(struct platform_device *pdev);
 
 struct lfa_device_data {
     struct cdev lfa_cdev;
+    dev_t dev_num;
+    int minor;
     atomic_t open_count;
     void __iomem *mmio_base;
 };
--- br2-external/linear_function_accelerator/src/lfa_device.c
+++ br2-external/linear_function_accelerator/src/lfa_device.c
@@ -3,12 +3,24 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/fs.h>
+#include <linux/idr.h>
 #include <linux/kdev_t.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 
-static dev_t dev_num;
+static dev_t lfa_base_dev_num;
 static struct class *dev_class;
+static bool chrdev_region_allocated;
+
+/* ID allocator for minor numbers */
+static DEFINE_IDA(lfa_minors);
+
+/* Maximum number of minor numbers */
+#define LFA_MAX_MINORS 256
+
+/* Mutex for protecting device registration */
+static DEFINE_MUTEX(lfa_device_lock);
 
 static const char *driver_name = "lfa";
 static const char *class_name = "lfa";
@@ -53,9 +65,12 @@ static const struct file_operations lfa_fops = {
 
 int lfa_register_device(struct platform_device *pdev) {
     int ret;
+    int minor;
     struct device *dev_ret;
     struct lfa_device_data *device_data;
+    struct resource *res;
 
+    /* Allocate and initialize device data struct */
     device_data =
         devm_kzalloc(&pdev->dev, sizeof(struct lfa_device_data), GFP_KERNEL);
     if (!device_data) {
@@ -63,71 +78,135 @@ int lfa_register_device(struct platform_device *pdev) {
     }
     atomic_set(&device_data->open_count, 0);
     device_data->mmio_base = NULL;
+    device_data->dev_num = 0;
+    device_data->minor = -1;
 
-    pr_info("INIT lfa driver\n");
-
-    /* Allocate device numbers (adds entry to /proc/devices) */
-    ret = alloc_chrdev_region(&dev_num, 0, 1, driver_name);
-    if (ret < 0) {
-        goto err_alloc_chrdev;
+    /* Get MMIO resource and map it:
+     * This maps the physical address range of the device into the kernel's
+     * virtual address space, allowing the driver to access the device's
+     * registers. The resource is defined in the device tree
+     */
+    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if (!res) {
+        return -ENODEV;
     }
-    pr_info("Major = %d Minor = %d\n", MAJOR(dev_num), MINOR(dev_num));
+    device_data->mmio_base = devm_ioremap_resource(&pdev->dev, res);
+    if (IS_ERR(device_data->mmio_base)) {
+        return PTR_ERR(device_data->mmio_base);
+    }
+
+    mutex_lock(&lfa_device_lock);
+
+    /* Allocate device numbers from (MAJOR, 0) to (MAJOR, LFA_MAX_MINORS-1):
+     * This only has to be done once for the entire driver, since we can use the
+     * minor number to differentiate between devices.
+     */
+    if (!chrdev_region_allocated) {
+        ret = alloc_chrdev_region(&lfa_base_dev_num, 0, LFA_MAX_MINORS,
+                                  driver_name);
+        if (ret < 0) {
+            goto err_unlock;
+        }
+        chrdev_region_allocated = true;
+    }
+
+    /* Create device class:
+     * This creates an entry in /sys/class/<class_name> and allows udev to
+     * create device nodes in /dev/ when we create devices below. We only need
+     * one class for all devices.
+     */
+    if (!dev_class) {
+        dev_class = class_create(class_name);
+        if (IS_ERR(dev_class)) {
+            ret = PTR_ERR(dev_class);
+            dev_class = NULL;
+            goto err_unregister_region;
+        }
+    }
+
+    /* Allocate a minor number for this device:
+     * This is done for each device instance and is used to create a unique
+     * device number for each device.
+     */
+    minor = ida_alloc(&lfa_minors, GFP_KERNEL);
+    if (minor < 0) {
+        ret = minor;
+        goto err_destroy_class;
+    }
+    device_data->minor = minor;
+    device_data->dev_num = MKDEV(MAJOR(lfa_base_dev_num), minor);
 
     /* Initialize and register character device */
     cdev_init(&device_data->lfa_cdev, &lfa_fops);
     device_data->lfa_cdev.owner = THIS_MODULE;
-    ret = cdev_add(&device_data->lfa_cdev, dev_num, 1);
+    ret = cdev_add(&device_data->lfa_cdev, device_data->dev_num, 1);
     if (ret < 0) {
-        goto err_cdev_add;
+        goto err_free_minor;
     }
 
-    /* Create device class (creates /sys/class/<class_name>/) */
-    dev_class = class_create(class_name);
-    if (IS_ERR(dev_class)) {
-        ret = PTR_ERR(dev_class);
-        goto err_class_create;
-    }
-
-    /* Create device (creates /sys/class/<class_name>/<device_name>/
-     * and triggers creation of /dev/<device_name> via udev) */
-    dev_ret = device_create(dev_class, NULL, dev_num, NULL, device_name);
+    /* Create a new device file:
+     * Create entry in /sys/class/<class_name>/<device_name>,
+     * triggers creation of /dev/<device_name> via udev
+     */
+    dev_ret = device_create(dev_class, &pdev->dev, device_data->dev_num, NULL,
+                            "%s%d", device_name, device_data->minor);
     if (IS_ERR(dev_ret)) {
         ret = PTR_ERR(dev_ret);
         goto err_device_create;
     }
 
-    struct resource *res;
-    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    if (!res) {
-        ret = -ENODEV;
-        goto err_device_create;
-    }
-    device_data->mmio_base = devm_ioremap_resource(&pdev->dev, res);
-    if (IS_ERR(device_data->mmio_base)) {
-        ret = PTR_ERR(device_data->mmio_base);
-        goto err_device_create;
-    }
+    /* Save device data for later use:
+     * It can be retrieved using `platform_get_drvdata`.
+     */
     platform_set_drvdata(pdev, device_data);
 
-    pr_info("Driver initialized successfully\n");
+    mutex_unlock(&lfa_device_lock);
+
+    pr_info("%s: initialized successfully (major=%d, minor=%d)\n", driver_name,
+            MAJOR(device_data->dev_num), MINOR(device_data->dev_num));
     return 0;
 
 err_device_create:
-    class_destroy(dev_class);
-err_class_create:
     cdev_del(&device_data->lfa_cdev);
-err_cdev_add:
-    unregister_chrdev_region(dev_num, 1);
-err_alloc_chrdev:
+err_free_minor:
+    ida_free(&lfa_minors, device_data->minor);
+err_destroy_class:
+    if (ida_is_empty(&lfa_minors) && dev_class) {
+        class_destroy(dev_class);
+        dev_class = NULL;
+    }
+err_unregister_region:
+    if (ida_is_empty(&lfa_minors) && chrdev_region_allocated) {
+        unregister_chrdev_region(lfa_base_dev_num, LFA_MAX_MINORS);
+        chrdev_region_allocated = false;
+    }
+err_unlock:
+    mutex_unlock(&lfa_device_lock);
     return ret;
 }
 
 void lfa_unregister_device(struct platform_device *pdev) {
     struct lfa_device_data *device_data = platform_get_drvdata(pdev);
 
-    device_destroy(dev_class, dev_num);
-    class_destroy(dev_class);
-    cdev_del(&device_data->lfa_cdev);
+    if (!device_data) {
+        return;
+    }
 
-    unregister_chrdev_region(dev_num, 1);
+    mutex_lock(&lfa_device_lock);
+
+    device_destroy(dev_class, device_data->dev_num);
+    cdev_del(&device_data->lfa_cdev);
+    ida_free(&lfa_minors, device_data->minor);
+
+    if (ida_is_empty(&lfa_minors) && dev_class) {
+        class_destroy(dev_class);
+        dev_class = NULL;
+    }
+    if (ida_is_empty(&lfa_minors) && chrdev_region_allocated) {
+        unregister_chrdev_region(lfa_base_dev_num, LFA_MAX_MINORS);
+        chrdev_region_allocated = false;
+    }
+
+    platform_set_drvdata(pdev, NULL);
+    mutex_unlock(&lfa_device_lock);
 }
